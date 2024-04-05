3f2453f3c20fe7c5137d1d5c470aba06897d86e2
Yujin <thfxm0601@gmail.com>
2024-04-05 13:16:04 +0900

    vue-cli

---
+++ vue-cli/src/AVue.vue
@@ -0,0 +1,21 @@
+<!-- var appHeader = { template: '
+<div>header</div>
+', methods:{ addNum: function() { } } } -->
+
+<!-- .vue 파일 구조 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  //Javascript (뷰 컴포넌트 내용) - 인스턴스 옵션(속성)
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style>
+/* CSS (뷰 템플릿의 스타일링) */
+</style>
---
+++ vue-cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div><AppHeader v-bind:propsdata="str" v-on:renew="renewStr" /></div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data: function () {
+    return {
+      str: "Header",
+    };
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "hihi";
+    },
+  },
+};
+</script>
+
+<style></style>
